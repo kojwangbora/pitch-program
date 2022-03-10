62cbb373a042b7e400824c72feca53ef8910bdd8
kojwangbora <kojwangbora254@gmail.com>
2022-03-10 18:32:26 +0300

    add config file

---
+++ manage.py
@@ -0,0 +1,19 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import Migrate, MigrateCommand
+from app.models import User
+
+#create app instance
+app = create_app
+manager = Manager(app)
+manager.add_command('server',Server)
+
+manager.add_command('db',MigrateCommand)
+migrate = Migrate(app,db)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app= app,db = db,User = User)
+if __name__ == '__main__':
+    manager.run()
