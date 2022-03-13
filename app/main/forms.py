32b9484e989ff145c80c8c753e83dd1115cad628
kojwangbora <kojwangbora254@gmail.com>
2022-03-13 07:52:45 +0300

    remove pitchform error, modify forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,33 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from app.models import User
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
 
 
 
+class PitchForm(FlaskForm):
+    pitch = TextAreaField('Enter your Pitch')
+    name = StringField('Enter your name', validators=[DataRequired()])
+    submit = SubmitField('Pitch', validators=[DataRequired()])
 
-class Register(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(message='Please enter your userame'),Length(min=4, max = 20)])
-    email = StringField("Email Address", validators= [DataRequired(),Email()])
-    password = PasswordField('Password', validators=[
-    DataRequired(), EqualTo('confirm_password', message='Passwords must match.')])    
-    confirm_password = PasswordField('confirm password', validators=[DataRequired()])
-    submit = SubmitField('Register')
-
-
-    def validate_user(self, field):
-        if User.query.filter_by(username = field.data).first():
-            raise ValidationError('User Already exists')
-
-
-    def validate_email(self,field):
-        if User.query.filter_by(email =field.data).first():
-            raise ValidationError('Email exists')
-
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Length(1, 64),
-    Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Keep me logged in')
-    submit = SubmitField('Log In')
+class EditProfile(FlaskForm):
+    name = StringField('Enter your name')
+    about_me = TextAreaField('Add bio')
+    submit = SubmitField('Submit')
