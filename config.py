1fb3448b0cecd98eaca99589c07a0dee825c8703
kojwangbora <kojwangbora254@gmail.com>
2022-03-13 07:45:48 +0300

    change configurations to production

--- config.py
+++ config.py
@@ -3,16 +3,30 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    
 
-
-class ProdConfig(Config):
-    pass
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard stuff to to crack'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 
 class DevConfig(Config):
-    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+
     DEBUG = True
+
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
+    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+           SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://','postgresql://',1)
+    
+    
 
 config_options = {
 'development':DevConfig,
